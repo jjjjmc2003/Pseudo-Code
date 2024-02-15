708f41a28ee232d12a96a2c4cd63eea7fb6a8161
jjjjmc2003 <j.mcintosh25@ncf.edu>
2024-02-15 11:19:37 -0500

    Initial commit

---
+++ Pseudo/Pseudo.java
@@ -0,0 +1,24 @@
+// Fibonachi Pseudocode
+
+// 1. Start
+// 2. Set Class Fibonachi
+// 3. Input as N (number of terms in the series), get from user
+// 4. Fib Series, create empty array to store the Fib series
+// 5. Fib series first number, set to 0
+// 6. Fib series second number, set to 1
+
+// 7. For loop to calculate the fib series
+// For loop would look something like this,
+// for (number = 2, number < N (user input), number increases every iteration)
+
+// 8. Now that we have a loop, we have to add our calculation
+
+// which would look like this:
+// series of number = sum of previous two numbers
+// add to array Fib Series
+
+// 9. Add if statement so we don't break it
+// if Fibseries length = N, break the loop
+
+// 10. Print Fib Series
+
